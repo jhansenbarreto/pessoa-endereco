4d96b95d6c7f88a71cb3d421ec1b681dcaff7c59
jhansenbarreto <jhansen_fera@hotmail.com>
2023-01-19 10:16:56 -0300

    :white_check_mark: Início da implementação dos testes JUnit

---
+++ src/test/java/com/attornatus/EstadoIT.java
@@ -0,0 +1,36 @@
+package com.attornatus;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class EstadoIT {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.port = port;
+        RestAssured.basePath = "/estados";
+    }
+    
+    @Test //Status OK == 200
+    public void deve_retornar_ok_na_consulta() {
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .when()
+                .get()
+                .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+}
