90127c43b015f05e662d80f17cc7c0948895c75f
Jhansen Barreto <jhansen.barreto@ftc.edu.br>
2023-10-19 21:37:13 -0300

    :bug: Correção de CORS

--- src/main/java/com/attornatus/PessoaEnderecoApplication.java
+++ src/main/java/com/attornatus/PessoaEnderecoApplication.java
@@ -1,5 +1,7 @@
 package com.attornatus;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  * @author Jhansen Barreto
  */
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class PessoaEnderecoApplication {
 
